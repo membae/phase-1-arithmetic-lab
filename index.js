ff9437af39432674928b2ef2961c124481ef4358
membae <membaphilip1@gmail.com>
2024-06-25 09:07:27 +0300

    arithmetic

--- index.js
+++ index.js
@@ -1,2 +1,15 @@
-
+let num1=31;
+let num2=2;
+let multiply=num1*num2;
+console.log(multiply);
 // Write your code here
+
+let random=Math.ceil(Math.random()*10);
+console.log(random);
+let num3=10;
+let num4=6;
+let mod=num3%num4;
+console.log(mod);
+
+let max=Math.max(1,2,4,5,12,20);
+console.log(max);
